406ebe29fb66510cc3dede1bad1083197ae17c6a
Joham Alvarez-Montoya <jalvarmon@gmail.com>
2023-06-19 20:41:35 -0500

    Refactor the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib import URLError
 
 streamlit.title("My Parents New Healthy Diner")
 
@@ -36,12 +37,6 @@ streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contain:")
-streamlit.text(my_data_row)
-
-
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
